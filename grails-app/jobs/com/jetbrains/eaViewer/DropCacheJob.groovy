5011bb9c4c99e43a6a99881997620f940d96857f
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2013-11-21 15:26:24 +0400

    cache queries

---
+++ grails-app/jobs/com/jetbrains/eaViewer/DropCacheJob.groovy
@@ -0,0 +1,23 @@
+package com.jetbrains.eaViewer
+
+/**
+ * @author Sergey Evdokimov
+ */
+class DropCacheJob {
+
+  static triggers = {
+    simple name: 'mySimpleTrigger', startDelay: 5*60*1000, repeatInterval: 5*60*1000
+  }
+
+  def sessionFactory
+
+  def execute() {
+    sessionFactory.getCache().evictQueryRegions()
+    sessionFactory.getCache().evictEntityRegions()
+    sessionFactory.getCache().evictCollectionRegions()
+    sessionFactory.getCache().evictDefaultQueryRegion()
+
+    downloadCountService.doUpdateDb()
+  }
+
+}
